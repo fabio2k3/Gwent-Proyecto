abc8fd44abdd1825bc7662eef8bc8e2a77857427
fabio2k3 <fvab2k3@gmail.com>
2024-03-07 04:42:25 +0100

    Edicionando Jugador 3D
    Dandole movilidad al jugador

---
+++ Gwent Proyecto Pro/Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    // Controlar la velocidad de nuestro personaje
+    public float runSpeed = 7;
+
+    // Controlar la velocidad al girar
+    public float rotationSpeed = 250;
+
+    public Animator animator;
+
+    private float x, y; // varaibles para saber en que eje nos estamos moviendo
+
+    void Update()
+    {
+        x = Input.GetAxis("Horizontal"); // saber si nos movemos a la izquierda o la derecha
+
+        y = Input.GetAxis("Vertical"); // saber si nos movemos hacia delante o hacia atrás
+
+        transform.Rotate(0,x*Time.deltaTime*rotationSpeed,0);
+
+        transform.Translate(0,0,y*Time.deltaTime*runSpeed);
+
+
+        animator.SetFloat("VelX", x);
+        animator.SetFloat("VelY",y);
+    }
+}

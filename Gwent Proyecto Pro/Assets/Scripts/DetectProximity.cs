fdf72ad6b1b51664dfa494c6e58794174c255513
fabio2k3 <fvab2k3@gmail.com>
2024-03-10 17:52:15 +0100

    Agregando Cartel para iniciar el juego 1.0

---
+++ Gwent Proyecto Pro/Assets/Scripts/DetectProximity.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DetectProximity : MonoBehaviour
+{
+    public GameObject messageText; 
+    public Transform cameraTransform; 
+
+    private bool collisionDetected = false; 
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("TablePlayer1") && !collisionDetected)
+        {
+            Vector3 position = cameraTransform.position + cameraTransform.forward * 3f; 
+            messageText.transform.position = position;
+            messageText.SetActive(true);
+            collisionDetected = true;
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("TablePlayer1"))
+        {
+            messageText.SetActive(false);
+            collisionDetected = false;
+        }
+    }
+}
+

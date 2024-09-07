ae1520051ea3f28ca0d38772a5806811b2830505
fabio2k3 <fvab2k3@gmail.com>
2024-09-07 18:43:32 +0200

    Agregando Nuevo Personaje

--- Gwent Proyecto Pro/Assets/Scripts/Segundo Proyecto/EvaluateMyEffects.cs
+++ Gwent Proyecto Pro/Assets/Scripts/Segundo Proyecto/EvaluateMyEffects.cs
@@ -30,8 +30,8 @@ public class EvaluateMyEffects : MonoBehaviour
     GameObject[,] myBoard = DragAndDrop.gameObjectsCards;
 
     // Carpeta donde se encuentran las cartas
-    private string pathTexts = "Assets/Textos/TextsEffects";
-    private string pathCards = "Assets/Textos/Used";
+    private string pathTexts = "Assets/Texts/TextsEffects";
+    private string pathCards = "Assets/Texts/Used";
     private string pathPrefabsOrc = "Assets/Prefabs/2nd Project/Created/Orc";
     private string pathPrefabsWarrior = "Assets/Prefabs/2nd Project/Created/Warrior";
 
@@ -39,6 +39,12 @@ public class EvaluateMyEffects : MonoBehaviour
     {
 
     } 
+
+
+    public void Check()
+    {
+        EvaluateEffectCards(pathPrefabsWarrior);
+    }
 
     // Metodo para Evaluar los efectos de mis Cartas en el juego (Recibe le path donde se
     //  encuentran mis cartas)
@@ -48,12 +54,12 @@ public class EvaluateMyEffects : MonoBehaviour
         string[] prefaFiles = Directory.GetFiles(path, "*.prefab");
 
         // Obtener los textos de los efectos
-        string[] effectsFiles = Directory.GetFiles(pathTexts, "*.prefab");
+        string[] effectsFiles = Directory.GetFiles(pathTexts, "*.txt");
 
         // Obtener los textos de las cartas
-        string[] textCards = Directory.GetFiles(path, "*.txt");
+        string[] textCards = Directory.GetFiles(pathCards, "*.txt");
 
-        // Si en la carptea hay elementos
+        // Si en la carptea hay elementos"*.txt"
         if (prefaFiles.Length > 0)
         {
             // Iterar por cada prefabs de mi carpeta
@@ -64,11 +70,14 @@ public class EvaluateMyEffects : MonoBehaviour
 
                 if (myPrefab != null) 
                 {
+                    Debug.Log("No es NULO");
                     // Iterar por cada texto de las cartas
                     foreach(string textCard in textCards)
                     {
+                        string textCardName = Path.GetFileNameWithoutExtension(textCard);
+
                         // Si el Nombre de algun texto coincide con el Nombre de mi carta
-                        if(textCard == myPrefab.GetComponent<Cards>().name)
+                        if(textCardName.Replace(" ", "") == myPrefab.GetComponent<Cards>().name.Replace(" ", ""))
                         {
                             // Obterner el texto de mi carta
                             string contentCard = File.ReadAllText(textCard);
@@ -86,8 +95,10 @@ public class EvaluateMyEffects : MonoBehaviour
                                 // Iterar por todos los textos de mis efectos
                                 foreach(string textsEffects in effectsFiles)
                                 {
+                                    string textCardEffect = Path.GetFileNameWithoutExtension(textsEffects);
+
                                     // Caso que coincida el texto de un efecto con el de la declaracion de mi carta
-                                    if(textsEffects == effectsOfCards.Effect.Name)
+                                    if (textCardEffect.Replace(" ", "") == effectsOfCards.Effect.Name.Replace(" ", ""))
                                     {
                                         // Obtener el texto de mi Efecto
                                         string contentEffect = File.ReadAllText(textsEffects);
@@ -98,8 +109,6 @@ public class EvaluateMyEffects : MonoBehaviour
 
                                         ParserEffect parserEffect = new ParserEffect(tokensEffect);
                                         Effect effectText = parserEffect.ParseEffect();
-
-                                        
                                     }
                                 }
                             }
